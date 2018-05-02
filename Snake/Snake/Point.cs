0e29507dfcd318b464cc4b0162bff5e1fd23ff08
MihailGolden <709@bk.ru>
2018-05-02 16:10:16 +0300

    Incapsulation

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -8,9 +8,16 @@ namespace Snake
 {
     class Point
     {
-        public int x;
-        public int y;
-        public char sym;
+        private int x;
+        private int y;
+        private char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
 
         public void Draw()
         {
